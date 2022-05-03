3e56e4b0f4aa9ccd7d5aed7ff02861885bf3d81a
Sorin Marcu <marcu.sorin.2003@gmail.com>
2022-05-03 15:26:20 +0300

    bug fixing

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,15 @@
-import * as flsFunctions from "./modules/functions.js";
-
-flsFunctions.isWebp();
+function isWebp() {
+  function testWebP(callback) {
+    let webP = new Image();
+    webP.onload = webP.onerror = function () {
+      callback(webP.height == 2);
+    };
+    webP.src =
+      "data:image/webp;base64,UklGRi4AAABXRUJQVlA4ICIAAABQAQCdASoDAAIAAgA2JQBOgC6gAP73M8eLuxHGTv3eIAAA";
+  }
+  testWebP(function (support) {
+    let className = support === true ? "webp" : "";
+    document.documentElement.classList.add(className);
+  });
+}
+isWebp();
